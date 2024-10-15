f2cafde5e078790d3bccc8f7fcffc3f89488a6ca
MauricioNoval <novalbarrera@gmail.com>
2024-10-15 09:11:10 -0300

    Calculadora

---
+++ Clase12/BoxColor/script.js
@@ -0,0 +1,8 @@
+function cambiarColor(){
+    const contenedor = document.getElementById("micontenedor");
+    contenedor.style.backgroundColor = contenedor.style.backgroundColor === "aqua" ? "green" : "aqua";
+}
+
+
+// a la calculadora agregale mas operaciones y que cambie de color.
+
